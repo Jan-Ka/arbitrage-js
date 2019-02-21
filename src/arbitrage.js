c3417210a0075c702f1542cd52d7883582f36622
Jan Ka <jan.kaltenecker@outlook.com>
2019-02-21 20:56:21 +0100

    renamed goods to wares refactored updating wares

--- src/arbitrage.js
+++ src/arbitrage.js
@@ -22,20 +22,20 @@ function onGameStartButtonClick() {
     renderLocationView();
 }
 
-function renderLocationView(name, goods) {
+function renderLocationView(name, wares) {
     if (typeof name !== "string") {
         name = "nowhere";
     }
 
-    if (!Array.isArray(goods)) {
-        goods = [...getGoods()];
+    if (!Array.isArray(wares)) {
+        wares = [...getWares()];
     }
 
     renderView(
         [
             ...getStatusElements.call(this),
             ["br"],
-            ...getBuyGoodsButtons.call(this, goods),
+            ...getBuyGoodsButtons.call(this, wares),
             ["br"],
             [
                 "button",
@@ -47,7 +47,7 @@ function renderLocationView(name, goods) {
         ],
         (gameState) => {
             gameState.locationName = name;
-            gameState.locationGoods = goods;
+            gameState.locationWares = wares;
         }
     );
 }
@@ -57,7 +57,7 @@ function renderLocationView(name, goods) {
  */
 function getShowInventoryClick() {
     return () => {
-        const goods = this.hasOwnProperty("goods") ? this.goods : {};
+        const wares = this.hasOwnProperty("wares") ? this.wares : {};
         const items = this.hasOwnProperty("items") ? this.items : {};
 
         renderView(
@@ -67,17 +67,17 @@ function getShowInventoryClick() {
                 [
                     "h2",
                     (elem) => {
-                        elem.textContent = "Goods";
+                        elem.textContent = "Wares";
                     }
                 ],
-                ...getGoodsList(goods),
+                ...getWaresList(wares),
                 ...getItemList(items),
                 [
                     "button",
                     (elem, gameState) => {
                         elem.textContent = "Back";
                         elem.onclick = () => {
-                            renderLocationView(gameState.locationName, gameState.locationGoods);
+                            renderLocationView(gameState.locationName, gameState.locationWares);
                         };
                     }
                 ]
@@ -121,15 +121,15 @@ function getBuyGoodsButtons(goods) {
     }, []);
 }
 
-function getGoodsList(goods) {
-    return Object.keys(goods).reduce((acc, goodsKey, i, arr) => {
-        const goodsCount = goods[goodsKey];
+function getWaresList(wares) {
+    return Object.keys(wares).reduce((acc, wareKey, i, arr) => {
+        const wareCount = wares[wareKey];
 
         acc.push(
             [
                 "span",
                 (elem) => {
-                    elem.textContent = `${goodsCount} x ${goodsKey}`;
+                    elem.textContent = `${wareCount} x ${wareKey}`;
                 }
             ]
         );
@@ -163,52 +163,104 @@ function getItemList(items) {
     }, []);
 }
 
-function getOnBuyGoodClick(good) {
+function getOnBuyGoodClick(ware) {
     return () => {
-        if (!this.hasOwnProperty("goods")) {
-            this.goods = {};
-        }
+        const hasEnoughMoney = (gameState, wareName, warePrice) => {
+            return gameState.money >= warePrice;
+        };
 
-        const goodName = good[0];
-        const goodPrice = good[1];
+        if (changeWareIfNotAvailable(this, ware, (gameState, wareName, warePrice) => {
+            gameState.wares[wareName] = 1;
 
-        if (this.money < goodPrice) {
+            gameState.money -= warePrice;
+        }, hasEnoughMoney)) {
+            renderLocationView(this.locationName, this.locationWares);
+
             return;
         }
 
-        if (this.goods.hasOwnProperty(goodName)) {
-            this.goods[goodName] += 1;
-        } else {
-            this.goods[goodName] = 1;
-        }
+        changeWaresIfAvailable(this, ware, (gameState, wareName, warePrice) => {
+            gameState.wares[wareName] += 1;
 
-        this.money -= goodPrice;
+            gameState.money -= warePrice;
+        }, hasEnoughMoney);
 
-        renderLocationView(this.locationName, this.locationGoods);
+        renderLocationView(this.locationName, this.locationWares);
     };
 }
 
 function getOnSellGoodClick(good) {
     return () => {
-        if (!this.hasOwnProperty("goods")) {
-            this.goods = {};
-        }
-
-        const goodName = good[0];
-        const goodPrice = good[1];
-
-        if (this.goods.hasOwnProperty(goodName)) {
-            if (this.goods[goodName] > 1) {
-                this.goods[goodName] -= 1;
+        changeWaresIfAvailable(this, good, (gameState, wareName, warePrice) => {
+            if (gameState.wares[wareName] > 1) {
+                gameState.wares[wareName] -= 1;
             } else {
-                delete this.goods[goodName];
+                delete gameState.wares[wareName];
             }
 
-            this.money += goodPrice;
-        }
+            this.money += warePrice;
+        });
 
-        renderLocationView(this.locationName, this.locationGoods);
+        renderLocationView(this.locationName, this.locationWares);
     };
+}
+
+function ensureWares(gameState) {
+    if (!gameState.hasOwnProperty("wares")) {
+        gameState.wares = {};
+    }
+}
+
+/**
+ * If current gameState has an entry for provided ware, call changeCallback
+ * @param {*} gameState
+ * @param {*} ware
+ * @param {(gameState, wareName, warePrice) => void} changeCallback
+ * @param {(gameState, wareName, warePrice) => boolean} predicate
+ * @returns {boolean} true if callback happened
+ */
+function changeWaresIfAvailable(gameState, ware, changeCallback, predicate) {
+    ensureWares(gameState);
+
+    if (typeof predicate === "function") {
+        if (!predicate.call(this, gameState, ...ware)) {
+            return false;
+        }
+    }
+
+    if (gameState.wares.hasOwnProperty(ware[0])) {
+        changeCallback.call(this, gameState, ...ware);
+    } else {
+        return false;
+    }
+
+    return true;
+}
+
+/**
+ * If current gameState has no entry for provided ware, call changeCallback
+ * @param {*} gameState
+ * @param {*} ware
+ * @param {(gameState, wareName, warePrice) => void} changeCallback
+ * @param {(gameState, wareName, warePrice) => boolean} predicate
+ * @returns {boolean} true if callback happened
+ */
+function changeWareIfNotAvailable(gameState, ware, changeCallback, predicate) {
+    ensureWares(gameState);
+
+    if (typeof predicate === "function") {
+        if (!predicate.call(this, gameState, ...ware)) {
+            return false;
+        }
+    }
+
+    if (!gameState.wares.hasOwnProperty(ware[0])) {
+        changeCallback.call(this, gameState, ...ware);
+    } else {
+        return false;
+    }
+
+    return true;
 }
 
 function* getStatusElements() {
@@ -229,7 +281,7 @@ function* getStatusElements() {
 
 // Game
 
-function* getGoods() {
+function* getWares() {
     yield [
         "Drug A",
         10
